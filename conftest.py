839a0e4d617734a036dc38a509a9910bb9e68771
Iceeze <danichPerm2016@mail.ru>
2025-12-01 10:52:00 +0500

    fix: реализация более корректного вывода ошибки при падении теста

--- conftest.py
+++ conftest.py
@@ -1,14 +1,15 @@
+import argparse
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser: argparse.ArgumentParser) -> None:
     parser.addoption("--language", action="store", default="en", help="Выберите язык отображения")
 
 
 @pytest.fixture(scope="function")
-def browser(request):
+def browser(request: pytest.fixture) -> webdriver.Chrome:
     language = request.config.getoption("language")
     options = Options()
     options.add_experimental_option('prefs', {'intl.accept_languages': language})
--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,19 @@
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
 class BasePage:
-    def __init__(self, browser, url):
+    def __init__(self, browser: webdriver, url: str, timeout: int = 10) -> None:
         self.browser = browser
         self.url = url
+        self.browser.implicitly_wait(timeout)
 
-    def open(self):
+    def open(self) -> None:
         self.browser.get(self.url)
+
+    def is_element_present(self, how: str, what: str) -> bool:
+        try:
+            self.browser.find_element(how, what)
+        except NoSuchElementException:
+            return False
+        return True
